945ee4a250c274ace0b418d315d4525dec54d60c
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-06-12 17:02:05 +0200

    aggiunte modifiche al codicenella creazione delle card

--- index.js
+++ index.js
@@ -2,41 +2,52 @@
 
 const fetchBooks = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
-    .then((responseObj) => {
-      console.log("response object", responseObj);
-      if (responseObj.ok) {
-        return responseObj.json();
+    .then((response) => {
+      console.log("response", response);
+      if (response.ok) {
+        return response.json();
       } else {
         throw new Error("Errore nel reperimento dei dati");
       }
     })
-    .then((booksObj) => {
-      console.log("ciao", booksObj);
+    .then((books) => {
+      //console.log("ciao", books);
       const row = document.getElementById("book-container");
-      booksObj.forEach((book) => {
+      books.forEach((book) => {
         const col = document.createElement("div");
         col.classList.add("col");
         const card = document.createElement("div");
         card.classList.add("card");
         const img = document.createElement("img");
-        img.classList.add(card - img - top);
+        img.classList.add("card-img-top");
         const cardBody = document.createElement("div");
-        cardBody.classList.add(card - body);
+        cardBody.classList.add("card-body");
         const title = document.createElement("h3");
-        title.classList.add(card - title);
+        title.classList.add("card-title");
         const category = document.createElement("p");
-        category.classList.add(card - text);
+        category.classList.add("card-text");
         const price = document.createElement("h4");
-        price.classList.add(card - text);
+        price.classList.add("card-text");
         const button = document.createElement("a");
-        button.classList.add(btn, btn - primary);
+        button.classList.add("btn", "btn-primary");
 
         img.setAttribute("src", `${book.img}`);
         img.setAttribute("alt", `${book.title}`);
+        card.style.maxHeigth = "200px";
         title.innerText = `${book.title}`;
         category.innerText = `${book.category}`;
         price.innerText = `${book.price}€`;
         button.innerText = "Scarta";
+
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        cardBody.appendChild(title);
+        cardBody.appendChild(category);
+        cardBody.appendChild(price);
+        cardBody.appendChild(button);
+        col.appendChild(card);
+        row.appendChild(col);
+        console.log(card);
       });
     })
     .catch((err) => console.log(err));
